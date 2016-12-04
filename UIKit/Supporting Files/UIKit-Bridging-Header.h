0ff65852434de1608e42a8cc3dba245cc83d1905
Andrey Ilskiy <nocross@users.noreply.github.com>
2016-12-04 15:55:50 +0300

    imported Supporting Files

---
+++ UIKit/Supporting Files/UIKit-Bridging-Header.h
@@ -0,0 +1,27 @@
+/*
+    Copyright (c) 2016 Andrey Ilskiy.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+ */
+
+#ifndef UIKit_Bridging_Header_h
+#define UIKit_Bridging_Header_h
+
+////! Project version number for UIKit.
+extern double getUIKitVersionNumber(void) __attribute__ ((used)) ;
+
+////! Project version string for UIKit.
+extern const unsigned char* getUIKitVersionString(void) __attribute__ ((used)) ;
+
+
+#endif /* UIKit_Bridging_Header_h */
