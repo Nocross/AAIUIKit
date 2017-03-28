847436829ba108c786b414ef15b9d3529d6a9574
Andrey Ilskiy <nocross@users.noreply.github.com>
2017-03-29 00:20:55 +0300

    implemented strong typed storyboard name

---
+++ UIKit/Sources/Extensions/UIKit/UIStoryboard+Name.swift
@@ -0,0 +1,39 @@
+/*
+    Copyright (c) 2017 Andrey Ilskiy.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+ */
+
+import UIKit
+
+extension UIStoryboard {
+    public struct Name: RawRepresentable, Hashable {
+        public private(set) var rawValue: String
+
+        public init?(rawValue: String) {
+            self.rawValue = rawValue
+        }
+
+        public var hashValue: Int {
+            return rawValue.hashValue
+        }
+
+        public static func ==(lhs: Name, rhs: Name) -> Bool {
+            return lhs.rawValue == rhs.rawValue
+        }
+    }
+
+    public convenience init(name: Name, bundle: Bundle? = nil) {
+        self.init(name: name.rawValue, bundle: bundle)
+    }
+}
