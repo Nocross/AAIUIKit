7c2f416f2b10c380a973912635b962b89dc0684e
Andrey Ilskiy <nocross@users.noreply.github.com>
2017-03-29 00:14:09 +0300

    adjusted framework versioning | implemented UIKit class

--- UIKit/Sources/UIKit.swift
+++ UIKit/Sources/UIKit.swift
@@ -1,11 +1,11 @@
 /*
-    Copyright (c) 2016 Andrey Ilskiy.
+    Copyright (c) 2017 Andrey Ilskiy.
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
     You may obtain a copy of the License at
 
-        http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
     Unless required by applicable law or agreed to in writing, software
     distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,15 +14,20 @@
     limitations under the License.
  */
 
-
-public final class Version {
+public final class UIKit {
     private init() {}
+}
 
-    public static var number: Double {
-        return getUIKitVersionNumber()
-    }
+public extension UIKit {
+    public final class Version {
+        private init() {}
 
-    public static var string: String {
-        return String(cString: getUIKitVersionString())
+        public static var number: Double {
+            return getUIKitVersionNumber()
+        }
+
+        public static var string: String {
+            return String(cString: getUIKitVersionString())
+        }
     }
 }
