c87e5dac77225bb7e79b77168b2209bdadfd68dc
Nocross <nocross@users.norepl:y.github.com>
2019-05-17 13:00:26 +0300

    added for uiview compressed size method | improved autoresizing mask(s) debug compabilty

--- UIKit/Sources/Extensions/UIKit/UIView+Extensions.swift
+++ UIKit/Sources/Extensions/UIKit/UIView+Extensions.swift
@@ -17,7 +17,7 @@
 
 import UIKit
 
-public extension UIView {
+extension UIView {
     @IBInspectable public var borderColor: UIColor? {
         get { return (self.layer.borderColor != nil) ? UIColor(cgColor: self.layer.borderColor!) : nil }
         set { self.layer.borderColor = newValue?.cgColor }
@@ -31,5 +31,68 @@ public extension UIView {
     @IBInspectable public var corderRadius: CGFloat {
         get { return self.layer.cornerRadius }
         set { self.layer.cornerRadius = newValue }
+    }
+}
+
+extension UIView {
+    public func compressSizeToFit(_ size: CGSize) {
+        var fittingSize = self.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+        
+        var bounds = CGRect(origin: CGPoint.zero, size: fittingSize)
+        self.bounds = bounds
+        
+        fittingSize = sizeThatFits(fittingSize)
+        bounds = CGRect(origin: CGPoint.zero, size: fittingSize)
+        
+        self.bounds = bounds
+    }
+}
+
+extension UIView.AutoresizingMask: CustomStringConvertible {
+    public var description: String {
+        let mirror = customMirror
+        
+        let result = mirror.children.reduce(into: "") {
+            if !$0.isEmpty { $0.append("+") }
+            
+            let description = String(describing: $1.value)
+            $0.append("\(description)")
+        }
+        
+        return result
+    }
+}
+
+extension UIView.AutoresizingMask: CustomReflectable {
+    public var customMirror: Mirror {
+        var result: Mirror
+        if isEmpty {
+            result = Mirror(self, unlabeledChildren: "None", displayStyle: .none)
+        } else {
+            var children = [String]()
+            
+            if contains(.flexibleLeftMargin) {
+                children.append("LM")
+            }
+            if contains(.flexibleWidth) {
+                children.append("W")
+            }
+            if contains(.flexibleRightMargin) {
+                children.append("RM")
+            }
+            if contains(.flexibleTopMargin) {
+                children.append("TM")
+            }
+            if contains(.flexibleHeight) {
+                children.append("H")
+            }
+            if contains(.flexibleBottomMargin) {
+                children.append("BM")
+            }
+            
+            result = Mirror(self, unlabeledChildren: children, displayStyle: .set)
+        }
+        
+        return result
     }
 }
